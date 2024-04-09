627fd7e31d74a13976c7448087fa8787a181ebfc
Guilherme Romero <guiromerolima08@hotmail.com>
2024-04-09 14:09:32 -0300

    fixing navbar

--- src/app/movimentacoes/page.tsx
+++ src/app/movimentacoes/page.tsx
@@ -1,10 +1,10 @@
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
 
-export default function Home() { 
-  return(
-    <main className="flex min-h-screen flex-col items-center ">
-     <NavBar />
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <NavBar active="movimentacoes" />
       <h2>Movimentações</h2>
     </main>
   );
